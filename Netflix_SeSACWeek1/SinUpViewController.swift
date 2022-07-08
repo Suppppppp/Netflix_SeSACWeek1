8f44d9da3ebad5b974647f10f21b768b987b1f71
Suppppppp <u1316@daum.net>
2022-07-08 20:31:45 +0900

    add seconview , button , gesture

--- Netflix_SeSACWeek1/SinUpViewController.swift
+++ Netflix_SeSACWeek1/SinUpViewController.swift
@@ -9,21 +9,88 @@ import UIKit
 
 class SinUpViewController: UIViewController {
 
+    @IBOutlet weak var emailAndPhonenumber_TextField: UITextField!
+    @IBOutlet weak var password_TextField: UITextField!
+    @IBOutlet weak var nickName_TextField: UITextField!
+    @IBOutlet weak var location_TextField: UITextField!
+    @IBOutlet weak var recommend_TextField: UITextField!
+    
+    @IBOutlet weak var switchOutlet: UISwitch!
+    @IBOutlet weak var signUpButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        basicSettingTo_TextField()
+        signUpButtonCustom()
+        switchSetting()
+        
+        emailAndPhonenumber_TextField.keyboardType = .numberPad
+  
+        
 
-        // Do any additional setup after loading the view.
+    }
+    
+    func basicSettingTo_TextField(){
+        
+
+        
+        let textField_OutletGroup : [UITextField :String] =
+        [
+            emailAndPhonenumber_TextField:"이메일주소 또는 전화번호",
+            password_TextField:"비밀번호",
+            nickName_TextField:"닉네임",
+            location_TextField:"위치",
+            recommend_TextField:"추천 코드 입력"
+        ]
+        
+        for (outlet,placeholder_message) in textField_OutletGroup {
+            
+            outlet.backgroundColor = .lightGray
+            outlet.textColor = .white
+            outlet.textAlignment = .center
+            outlet.borderStyle = .roundedRect
+
+            outlet.attributedPlaceholder =
+            NSAttributedString (
+                string: placeholder_message,
+                attributes:[NSAttributedString.Key.foregroundColor: UIColor.white]
+            )
+            
+            
+            
+    //        placeholder, keyboardType, isSecureTextEntry,
+        }
+        
+
+        
     }
     
 
-    /*
-    // MARK: - Navigation
+    func switchSetting() {
+        
+        switchOutlet.onTintColor = .red
+        switchOutlet.tintColor = .yellow
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        
     }
-    */
+    func signUpButtonCustom(){
+        
+        signUpButton.setTitleColor(.black, for: .normal)
+        signUpButton.setTitle("회원가입", for: .normal)
+        signUpButton.layer.cornerRadius = 4
+    }
+    
 
+    
+ 
+    @IBAction func touchGesture(_ sender: UITapGestureRecognizer) {
+        
+        view.endEditing(true)
+    }
+    
+    @IBAction func signUpButton_Action(_ sender: UIButton) {
+        view.endEditing(true)
+    }
+    
 }
